bc172a2bfc83668df1849051a05e98250d9f3819
JAMES <48850795+J4M35-P20L@users.noreply.github.com>
2021-02-25 12:52:38 +0530

    version2
    version2

---
+++ default.js
@@ -0,0 +1,20 @@
+function openNav()
+{
+  document.getElementById("myNav").style.width = "100%";
+}
+function closeNav()
+ {
+  document.getElementById("myNav").style.width = "0%";
+}
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+function clearx()
+ {
+  document.getElementById("clr").style.display='none';
+}
+function showx()
+ {
+ document.getElementById("myNav").style.width = "0%";
+  document.getElementById("clr").style.display='block';
+}
